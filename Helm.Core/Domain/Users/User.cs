782e07fa95570b7d513cbea122016253ee5d29bd
Pavel Milokum <pavelmilokum@gmail.com>
2026-02-25 21:43:03 +0300

    first commit

---
+++ Helm.Core/Domain/Users/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helm.Core.Domain.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Hash { get; set; }
+        public string Name { get; set; }
+        public string ADLogin { get; set; }
+        public bool Enabled { get; set; }
+        public bool Deleted { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
---
+++ Helm.Core/Domain/Users/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helm.Core.Domain.Users
+{
+    [Flags]
+    public enum UserRole
+    {
+        None = 0b_0000_0000,
+        CanEditUsers = 0b_0000_0001,
+
+    }
+}
